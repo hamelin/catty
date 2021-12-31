61771d00fba0eed8305d85a4a0ece71bb4fe9da1
Benoit Hamelin <benoit@benoithamelin.com>
2021-12-31 15:37:30 -0500

    Stack reduction basics

--- catty/__init__.py
+++ catty/__init__.py
@@ -1,5 +1,28 @@
+import inspect as ins
 from typing import *
 
 
-def reduce(stack: Iterable) -> Sequence:
-    return tuple(s for s in stack)
+def reduce(program: Iterable) -> Sequence:
+    stack: List = []
+    for word in program:
+        if hasattr(word, "__call__"):
+            sig = ins.signature(word)
+            num_args = len([
+                p
+                for p in sig.parameters.values()
+                if (
+                    p.kind in {
+                        ins.Parameter.POSITIONAL_ONLY,
+                        ins.Parameter.POSITIONAL_OR_KEYWORD
+                    } and p.default is ins.Parameter.empty
+                )
+            ])
+            if num_args == 0:
+                args = []
+            else:
+                args = stack[-num_args:]
+                del stack[-num_args:]
+            stack.append(word(*args))
+        else:
+            stack.append(word)
+    return stack
--- test/test_reduce.py
+++ test/test_reduce.py
@@ -1,12 +1,41 @@
+from operator import add
 from typing import *  # noqa
 
 from catty import reduce
 
 
 def test_reduce_empty():
-    assert reduce([]) == ()
+    assert reduce([]) == []
 
 
 def test_reduce_constants():
-    assert reduce([0]) == (0,)
-    assert reduce([1, None, "asdf"]) == (1, None, "asdf")
+    assert reduce([0]) == [0]
+    assert reduce([1, None, "asdf"]) == [1, None, "asdf"]
+
+
+def test_reduce_complexes():
+    assert reduce((1, [4, 90, [None]], {"a": 90, "b": 12}, (3,))) == [
+        1,
+        [4, 90, [None]],
+        {"a": 90, "b": 12},
+        (3,)
+    ]
+
+
+def test_operation_simple():
+    assert reduce([-3, 1, 3, add]) == [-3, 4]
+
+
+def test_operation_simple2():
+    assert reduce([1, 3, add, 2, add, 8]) == [6, 8]
+
+
+def test_no_input():
+    def thousand() -> int:
+        return 1000
+
+    assert reduce([None, 3, thousand, 8]) == [None, 3, 1000, 8]
+
+
+def test_quote():
+    assert reduce([-3, 1, 3, [add]]) == [-3, 1, 3, [add]]
