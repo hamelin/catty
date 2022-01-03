a67f5457f7875dbc363554d0f7f4cdd2ceab67ab
Benoit Hamelin <benoit@benoithamelin.com>
2022-01-02 21:01:48 -0500

    Some failure unit tests for Python function calls

--- test/test_integration_python.py
+++ test/test_integration_python.py
@@ -10,8 +10,20 @@ def test_callN_happy():
     check_reduce([23, 1, 2, 3, callN(plonk, 3)], [23, ([1, 2, 3], {})])
 
 
+def test_callN_insufficient():
+    check_error([1, 2, callN(plonk, 3)], IndexError)
+
+
 def test_callargs_happy():
     check_reduce([23, (1, 2, 3), callargs(plonk)], [23, ([1, 2, 3], {})])
+
+
+def test_callargs_insufficient():
+    check_error([callargs(plonk)], IndexError)
+
+
+def test_callargs_invalid():
+    check_error([3, callargs(plonk)], ValueError)
 
 
 def test_callstar_happy():
@@ -19,6 +31,18 @@ def test_callstar_happy():
         [23, [1, 2, 3], {"a": 123, "b": 456}, callstar(plonk)],
         [23, ([1, 2, 3], {"a": 123, "b": 456})]
     )
+
+
+def test_callstar_insufficient():
+    check_error([(), callstar(plonk)], IndexError)
+
+
+def test_callstar_wrong_args():
+    check_error([None, {}, callstar(plonk)], ValueError)
+
+
+def test_callstar_wrong_kwargs():
+    check_error([(4, 5), [0, 1, 2], callstar(plonk)], ValueError)
 
 
 def test_call_happy():
@@ -34,3 +58,7 @@ def test_call_happy():
         ],
         [23, ([1, 2, 3], {"a": 123, "b": 456})]
     )
+
+
+def test_call_missing_ref():
+    check_error([0, 1, call(plonk, St.post)], IndexError)
